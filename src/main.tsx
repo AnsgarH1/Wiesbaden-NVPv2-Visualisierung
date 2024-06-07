6abd576defa5e60fea980d9edc7d54f12429975e
Ansgar Hoyer <ansgar.hoyer@gmail.com>
2024-06-07 12:04:31 +0200

    fix: add sentry error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { MapProvider } from "react-map-gl";
 
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
@@ -17,7 +17,7 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", ],
+  tracePropagationTargets: ["localhost"],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
@@ -25,11 +25,13 @@ Sentry.init({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Analytics />
-    <ThemeProvider defaultTheme="dark">
-      <MapProvider>
-        <App />
-      </MapProvider>
-    </ThemeProvider>
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <Analytics />
+      <ThemeProvider defaultTheme="dark">
+        <MapProvider>
+          <App />
+        </MapProvider>
+      </ThemeProvider>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
